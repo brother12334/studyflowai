c8a59ac6a12d7765ba2b1b9588adb76c3a43aab1
brother12334 <ferbyablon@gmail.com>
2026-05-27 12:37:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // =========================
 // SUPABASE CONFIG
 // =========================
-const SUPABASE_URL  = "https://YOUR_PROJECT.supabase.co";
-const SUPABASE_ANON = "YOUR_ANON_KEY";
+const SUPABASE_URL  = "https://pdteyiowlyvlqdqdhhkg.supabase.co";
+const SUPABASE_ANON = "sb_publishable_RUhKJwZqo9L9dFVcTU3eaA_mrydsyvq";
 
 // =========================
 // AUTH & SYNC
